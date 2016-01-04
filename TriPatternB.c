0c970aa4b2f389c5d2d4928fe7111c3b279d6fd3
Mritika <mrithya.ramesh@gmail.com>
2016-01-04 20:17:30 +0530

    Update TriPatternB.c

--- TriPatternB.c
+++ TriPatternB.c
@@ -10,7 +10,7 @@ int main(void)
         for(j=1;j<=i;j++)
         {
             value=value*(i-j+1)/j;
-             printf("%d",value);
+             printf("%d\t",value);
         }
         printf("\n");
     }
