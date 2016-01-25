edb9f533ed5fd147b4d4a9566d6ab5e2b449d5b9
Mritika <mrithya.ramesh@gmail.com>
2016-01-25 22:01:47 +0530

    Update DecimalConv.c

--- DecimalConv.c
+++ DecimalConv.c
@@ -2,22 +2,23 @@
 void convert(int , int);
 int main()
 {
-    int num,base;
+    	int num,base;
 	printf("-- Decimal To Any Base Up to 36 (0-Z) --\n");
 	printf("Enter the number ");
 	scanf("%d",&num);
 	printf("Enter the base ");
 	scanf("%d",&base);
-	if(num < 0) 
+	if (num<0) 
 	{
         printf("Cannot convert negative numbers.");
         return 0;
-	}    
-    if(base==0 || base==1) 
+    	}  
+     
+    	if ( base==0 || base==1 ) 
 	{
-        printf("Cannot have 0 or 1 as a base.");
-        return 0;
-    } 
+		printf("Cannot have 0 or 1 as a base.");
+       		return 0;
+    	} 
 	convert(num,base);
 	return 0;
 }
@@ -40,10 +41,8 @@ void convert(int num , int radix)
 		i++;
 		num= num / radix;
 	}
-    for(j=i-1;j>=0;j--)
+	for(j=i-1;j>=0;j--)
 	{
 		printf("%c",conv[j]);
 	}
 }
-
-	
