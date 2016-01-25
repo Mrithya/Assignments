53b25a5d911e0fe2652a509260e26bf86a8168d5
Mritika <mrithya.ramesh@gmail.com>
2016-01-25 22:02:09 +0530

    Update DecimalConv.c

--- DecimalConv.c
+++ DecimalConv.c
@@ -10,8 +10,8 @@ int main()
 	scanf("%d",&base);
 	if (num<0) 
 	{
-        printf("Cannot convert negative numbers.");
-        return 0;
+        	printf("Cannot convert negative numbers.");
+        	return 0;
     	}  
      
     	if ( base==0 || base==1 ) 
