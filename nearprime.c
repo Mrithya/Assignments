8b0fec0ebbcb8a3929e7f6bf7357012bc5c17bbb
Mritika <mrithya.ramesh@gmail.com>
2016-02-05 19:44:29 +0530

    Create nearprime.c

---
+++ nearprime.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,count,num;
+	printf("nEnter the number");
+	scanf("%d",&num);
+	for(i=num+1;;i++)
+	{
+		for(j=2;j<=i;j++)
+		{
+ 			count=0;
+            if(i%j == 0)
+            {
+            	count++;
+            }      
+		} 
+ 		if(count==1)
+		{
+  			printf("%d",i);
+ 			break;            
+ 		}
+	}
+}
