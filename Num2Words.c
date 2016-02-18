15bdae506d85357063b13a6d637269817adf7aef
Mritika <mrithya.ramesh@gmail.com>
2016-02-18 19:35:54 +0530

    Update Num2Words.c

--- Num2Words.c
+++ Num2Words.c
@@ -1,17 +1,30 @@
 #include<stdio.h>
 int main()
 {
-	int num,m,n,k,i,j;
+	int num,m,n,k,i,j,l,temp,s;
 	char *units[]={"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
 	char *tens[] = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen","Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
 	char *hundreds[] = {"Twenty", "Thirty", "Forty", "Fifty","Sixty", "Seventy", "Eighty", "Ninety"};
 	
 	scanf("%d",&num);
-
+	temp=num;
+	
+	l=temp%100;
+	s=l%10;
+	
 	k=num/100;
 	if(num>99 && num<999)
-	printf(" %s hundred and  ",units[k]);
-	
+	{
+		if(s==0)
+		{
+			printf(" %s hundred ",units[k]);
+		}
+		else
+		{
+			printf(" %s hundred and",units[k]);
+		}
+	}
+
 	m=num%100;
 	i=m/10;
 	j=m%10;
@@ -19,15 +32,15 @@ int main()
 	{
 		if(i==1)
 		{
-			printf("%s",tens[j]);
+			printf(" %s ",tens[j]);
 			return 0;
 		}
 	}
 	else
 	{
-		printf("%s\t",hundreds[i-2]);
+		printf(" %s ",hundreds[i-2]);
 	}
 	n=num%10;
-	printf("%s",units[n]);
+	printf(" %s ",units[n]);
 	return 0;
 }
